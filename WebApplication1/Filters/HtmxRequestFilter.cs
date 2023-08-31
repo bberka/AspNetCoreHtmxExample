63ea8e60cd2d1bc8b51ac5f54735261a81581708
berkaybyr <berkaybyr@icloud.com>
2023-08-31 16:10:51 +0300

    Initial commit

---
+++ WebApplication1/Filters/HtmxRequestFilter.cs
@@ -0,0 +1,41 @@
+﻿using Htmx;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebApplication1.Filters;
+
+
+//FOR MVC
+// public class HtmxRequestActionFilter : ActionFilterAttribute
+// {
+//   public override void OnActionExecuting(ActionExecutingContext context) {
+//     var isHtmxRequest = context.HttpContext.Request.IsHtmx();
+//     if (!isHtmxRequest) {
+//       context.Result = new NoContentResult();
+//     }
+//   }
+//   
+// }
+
+
+//FOR RAZOR PAGES
+[AttributeUsage(AttributeTargets.Class)]
+public class HtmxRequestFragmentFilter : Attribute,IAsyncPageFilter
+{
+
+  public Task OnPageHandlerSelectionAsync(PageHandlerSelectedContext context) {
+    return Task.CompletedTask;
+  }
+  
+
+  public Task OnPageHandlerExecutionAsync(PageHandlerExecutingContext context, PageHandlerExecutionDelegate next) {
+    //Returns not found if request is not from htmx
+    //Remember this can easily be bypassed by changing the request header to htmx headers
+    var isHtmxRequest = context.HttpContext.Request.IsHtmx();
+    if (!isHtmxRequest) {
+      context.Result = new NotFoundResult();
+      return Task.CompletedTask;
+    }
+    return next();
+  }
+}
---
+++ WebApplication1/Pages/Fragments/Counter.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication1.Filters;
+
+namespace WebApplication1.Pages.Fragments;
+
+[HtmxRequestFragmentFilter]
+public class Counter : PageModel
+{
+  public static int StaticCount = 0;
+
+  [BindProperty]
+  public int Count { get; set; } = 0;
+
+  public void OnGet() {
+    StaticCount++;
+    Count += 2;
+  }
+}
